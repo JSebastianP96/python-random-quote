0d6cdd53350958c8005d03e17fd40d860dad7540
Sebastian Parra <ing.jsebastian.parra@gmail.com>
2020-07-07 12:50:42 -0500

    add elment to array, random array

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
+import random
 def principal():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  star = 0
+  last = len(quotes)-1
+  rnd = random.randint(star,last)
+  print(rnd)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   principal()
